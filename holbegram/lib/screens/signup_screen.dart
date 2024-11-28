a73d89fc1c8828f86236870cd26f31dde0072a47
Shane35300 <6874@holbertonstudents.com>
2024-11-28 15:03:23 +0100

    task 3 done

--- holbegram/lib/screens/signup_screen.dart
+++ holbegram/lib/screens/signup_screen.dart
@@ -37,6 +37,7 @@ class _SignUpState extends State<SignUp> {
     super.dispose();
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
@@ -51,7 +52,6 @@ class _SignUpState extends State<SignUp> {
                           fontFamily: 'Billabong',
                           fontSize: 50,
                         )),
-                    const SizedBox(height: 10),
                     // Logo
                     Image.asset(
                       'assets/images/logo.webp',
@@ -66,6 +66,104 @@ class _SignUpState extends State<SignUp> {
                         fontFamily: 'InstagramSans',
                         fontSize: 20,
                       ),
+                    ),
+                    const SizedBox(height: 22),
+                    TextFieldInput(
+                      controller: widget.emailController,
+                      isPassword: false,
+                      hintText: "Email",
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(height: 18),
+                    TextFieldInput(
+                      controller: widget.usernameController,
+                      isPassword: false,
+                      hintText: "Full Name",
+                      keyboardType: TextInputType.text,
+                    ),
+                    const SizedBox(height: 18),
+                    TextFieldInput(
+                      controller: widget.passwordController,
+                      isPassword: !_passwordVisible,
+                      hintText: "Password",
+                      keyboardType: TextInputType.visiblePassword,
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _passwordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: const Color.fromARGB(218, 226, 37, 24),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _passwordVisible =
+                                !_passwordVisible; // Toggle la visibilité du mot de passe
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 18),
+                    TextFieldInput(
+                      controller: widget.passwordController,
+                      isPassword: !_passwordVisible,
+                      hintText: "Confirm Password",
+                      keyboardType: TextInputType.visiblePassword,
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _passwordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: const Color.fromARGB(218, 226, 37, 24),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _passwordVisible =
+                                !_passwordVisible; // Toggle la visibilité du mot de passe
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 28),
+                    SizedBox(
+                      height: 48,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(
+                              const Color.fromARGB(218, 226, 37, 24)),
+                          shape: WidgetStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(5), // Coins carrés
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          // Actions de connexion à implémenter plus tard
+                        },
+                        child: const Text(
+                          'Sign up',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    const Divider(
+                      thickness: 2,
+                    ),
+                    const SizedBox(height: 28),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Have an account?'),
+                        Text(
+                          '   Log in',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(218, 226, 37, 24),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ))));
